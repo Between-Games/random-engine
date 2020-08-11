b36eafb6a0ae71723dee34fffc600069c8844bc4
S-A-R-C <s-a-r-c@s-a-r-c.com>
2020-08-11 08:29:09 -0400

    Created a brand new function to get a random digit

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,7 +1,9 @@
 export function getRandomBit(): number;
+export function getRandomDigit(): number;
 export function getRandomInteger(minimum: number, maximum: number): number;
 
 declare module 'RandomEngine' {
     export function getRandomBit(): number;
+    export function getRandomDigit(): number;
     export function getRandomInteger(minimum: number, maximum: number): number;
 }
--- src/index.js
+++ src/index.js
@@ -23,8 +23,19 @@ import UtilityEngine from '@sarc-test/utility-engine';
 // ║ ╚═══╝ ║╔══╝ ╚══╗   ║ ║
 // ╚═══════╝╚═══════╝   ╚═╝
 
-export function getRandomBit() {        // Get a random bit based on tge Math.random function
-    return (Math.random() < 0.5) * 1;   // Get a random bit based on tge Math.random function
+export function getRandomBit() {        // Get a random bit based on the Math.random function
+    return (Math.random() < 0.5) * 1;   // Get a random bit based on the Math.random function
+}
+
+// ╔═══════╗╔═══════╗╔═══════╗╔═══════╗╔═══════╗
+// ╚╗ ╔══╗ ║╚══╗ ╔══╝║ ╔═════╝╚══╗ ╔══╝╚══╗ ╔══╝
+//  ║ ║  ║ ║   ║ ║   ║ ║ ╔═══╗   ║ ║      ║ ║
+//  ║ ║  ║ ║   ║ ║   ║ ║ ╚═╗ ║   ║ ║      ║ ║
+// ╔╝ ╚══╝ ║╔══╝ ╚══╗║ ╚═══╝ ║╔══╝ ╚══╗   ║ ║
+// ╚═══════╝╚═══════╝╚═══════╝╚═══════╝   ╚═╝
+
+export function getRandomDigit() {  // Get random digit based on the Math.random function
+    return getRandomInteger(0, 9);  // Get random digit based on the Math.random function
 }
 
 // ╔═══════╗╔════╗╔═╗╔═══════╗╔═══════╗╔═══════╗╔═══════╗╔═══════╗
@@ -36,9 +47,9 @@ export function getRandomBit() {        // Get a random bit based on tge Math.ra
 
 export function getRandomInteger(minimum, maximum) {                                                    //
     if (!UtilityEngine.isNumber(minimum, Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER)) {           //
-        throw TypeError('the specified minimum parameter must be a safe integer');                      //
+        throw TypeError('The specified minimum parameter must be a safe integer');                      //
     } else if (!UtilityEngine.isNumber(maximum, Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER)) {    //
-        throw TypeError('the specified maximum parameter must be a safe integer');                      //
+        throw TypeError('The specified maximum parameter must be a safe integer');                      //
     }  else if (!UtilityEngine.isNumber(maximum, minimum)) {                                            //
         throw RangeError('The maximum parameter must be bigger than minimum parameter');                //
     } else return Math.round(Math.random() * (maximum - minimum)) + minimum;                            //
@@ -53,6 +64,8 @@ export function getRandomInteger(minimum, maximum) {                            
 
 export default {
     getRandomBit,
+
+    getRandomDigit,
 
     getRandomInteger,
 }
